26cc39df4913ed45545ddd2238f3d95cd7246027
AdriGeaPY <adrian.gea@iesjoandaustria.org>
2020-10-28 11:31:14 +0000

    bien chicos

---
+++ 4sm.py
@@ -0,0 +1,3 @@
+a=int(input("¿Cual es el numero de tu DNI?"))
+b=["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"] 
+print("Su numeros y letra del DNI es: ",a,b[a%23])
--- primer.py
+++ primer.py
@@ -1,21 +1,2 @@
-a=int(input("¿Que cara del dado escoges?: "))
-if a<=0 or a>=7:
-  print("Introduce unos valores correctos")
-elif a==1:
-  print("\nel numero que has escogido es el uno")
-  print("\ncara opuesta: ", 7-a)
-elif a==2:
-  print("\nel numero que has escogido es el dos")
-  print("\ncara opuesta: ", 7-a)
-elif a==3:
-  print("\nel numero que has escogido es el tres")
-  print("\ncara opuesta: ", 7-a)
-elif a==4:
-  print("\nel numero que has escogido es el cuatro")
-  print("\ncara opuesta: ", 7-a)
-elif a==5:
-  print("\nel numero que has escogido es el cinco")
-  print("\ncara opuesta: ", 7-a)
-elif a==6:
-  print("\nel numero que has escogido es el seis")
-  print("\ncara opuesta: ", 7-a)
+print("Vamos a jugar a cara o cruz, escoge un numero: ")
+print("")
